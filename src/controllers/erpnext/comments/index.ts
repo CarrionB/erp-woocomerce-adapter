214155878d12b88c18dd6a6dee7c1f7cff7f9d12
CarrionB <ssj4_bryan@yahoo.es>
2022-03-14 13:01:47 -0500

    feat: :necktie: added link to subscription on comment

--- src/controllers/erpnext/comments/index.ts
+++ src/controllers/erpnext/comments/index.ts
@@ -2,17 +2,21 @@ import axios from "axios"
 
 const {ERP_URL} = process.env
 
+const SUBCRIPTION_DOC_URL = `${ERP_URL}/app/subscription/`
+
 const ADD_COMMENTS_URL = `${ERP_URL}/api/method/frappe.desk.form.utils.add_comment`
 
 export const addCommentToOrder = async(
   subscriptionData: any, salesOrderId: string, cookieId: string
 ) => {
 
-  const {start_date, end_date, plans} = subscriptionData
+  const {name, start_date, end_date, plans} = subscriptionData
+
+  const linkToSub = `<a href=\"${SUBCRIPTION_DOC_URL}${name}\">View subcription</a>`
 
   const subPlanName = plans[0].plan
 
-  const comment = `<div class=\"ql-editor+read-mode\"><p>Subcription: ${subPlanName}</p><p>Start date: ${start_date}</p><p>End date: ${end_date}</p></div>`
+  const comment = `<div class=\"ql-editor+read-mode\"><p>Subcription: ${subPlanName}</p><p>Start date: ${start_date}</p><p>End date: ${end_date}</p>${linkToSub}</div>`
 
   const data = {
     reference_doctype: "Sales Order",
