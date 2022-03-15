7b6f88a3ce66e8e29996cec7dada4915468707a8
CarrionB <ssj4_bryan@yahoo.es>
2022-03-15 16:32:00 -0500

    feat: :sparkles: Added functionto comment user group on order

--- src/controllers/erpnext/comments/index.ts
+++ src/controllers/erpnext/comments/index.ts
@@ -6,7 +6,7 @@ const SUBCRIPTION_DOC_URL = `${ERP_URL}/app/subscription/`
 
 const ADD_COMMENTS_URL = `${ERP_URL}/api/method/frappe.desk.form.utils.add_comment`
 
-export const addCommentToOrder = async(
+export const addSubscriptionCommentToOrder = async(
   subscriptionData: any, salesOrderId: string, cookieId: string
 ) => {
 
@@ -18,6 +18,33 @@ export const addCommentToOrder = async(
 
   const comment = `<div class=\"ql-editor+read-mode\"><p>Subcription: ${subPlanName}</p><p>Start date: ${start_date}</p><p>End date: ${end_date}</p>${linkToSub}</div>`
 
+  const data = {
+    reference_doctype: "Sales Order",
+    reference_name: salesOrderId,
+    content: comment,
+    comment_email: "Administrator",
+    comment_by: "Administrator"
+  }
+
+  const resp = await axios({
+    method: 'POST',
+    url: `${ADD_COMMENTS_URL}`,
+    data,
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json',
+      'Cookie': cookieId
+    }
+  })
+
+  return resp.data.data
+}
+
+export const addCustomerGroupCommentToOrder = async(
+  salesOrderId: string, customerGroup: string, cookieId: string
+) => {
+  const comment = `<div class=\"ql-editor+read-mode\"><p>WooComerce group: ${customerGroup}</p></div>`
+  
   const data = {
     reference_doctype: "Sales Order",
     reference_name: salesOrderId,
