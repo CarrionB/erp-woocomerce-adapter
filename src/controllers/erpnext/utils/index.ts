03fa45e46f0c1b5a8123956420c57757b2eba3c0
Bryan Carrion <carrion.ba.dev@gmail.com>
2023-02-03 13:03:18 -0500

    fix: :bug: fix cookie test

--- src/controllers/erpnext/utils/index.ts
+++ src/controllers/erpnext/utils/index.ts
@@ -2,13 +2,14 @@ import { storage } from "../../../utilities/storage";
 
 export const isCookieInvalid = () => {
   const storedCookie = storage.getItem("cookieId");
+
   if (storedCookie === null) {
     return Boolean(storedCookie);
   }
+
   const storedExpDateString = storage.getItem("expirationDateString");
   const expirationDate = new Date(storedExpDateString);
   const isExpired = new Date().getTime() > expirationDate.getTime();
   
-
   return isExpired;
 };
