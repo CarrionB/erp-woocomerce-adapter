dcbff3c18842ff174578e615dcb2d55a887b27ae
Bryan Carrion <carrion.ba.dev@gmail.com>
2023-02-03 12:42:34 -0500

    fix: :bug: login bug

--- src/controllers/erpnext/utils/index.ts
+++ src/controllers/erpnext/utils/index.ts
@@ -2,13 +2,13 @@ import { storage } from "../../../utilities/storage";
 
 export const isCookieInvalid = () => {
   const storedCookie = storage.getItem("cookieId");
+  if (storedCookie === null) {
+    return Boolean(storedCookie);
+  }
   const storedExpDateString = storage.getItem("expirationDateString");
   const expirationDate = new Date(storedExpDateString);
   const isExpired = new Date().getTime() > expirationDate.getTime();
   
-  if (!storedCookie) {
-    return isExpired;
-  }
 
   return isExpired;
 };
