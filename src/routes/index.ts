1cad33d84b88cf564f3adf3fb95d44c6cbd778ae
Bryan Carrion <carrion.ba.dev@gmail.com>
2023-05-10 10:05:56 -0500

    fix: :memo: updated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -18,25 +18,19 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const routes = (app: Express) => {
   app.get("/", testFunction);
 
-  app.post("/", jsonParser, testFunction);
-
-  app.post("/item_creation", urlencodedParser, createWooComerceProduct);
+  app.post("/item/create", urlencodedParser, createWooComerceProduct);
   
-  app.post("/item_update", urlencodedParser, updateWooComerceProduct);
+  app.post("/item/update", urlencodedParser, updateWooComerceProduct);
 
-  app.post("/item_deletion", urlencodedParser, deleteWooComerceProduct);
+  app.post("/item/delete", urlencodedParser, deleteWooComerceProduct);
   
-  app.post("/item_category_creation", urlencodedParser, createWoocomerceProductCategory);
+  app.post("/item/category/create", urlencodedParser, createWoocomerceProductCategory);
   
-  app.post("/item_category_deletion", urlencodedParser, deleteWooComerceProductCategory);
+  app.post("/item/category/delete", urlencodedParser, deleteWooComerceProductCategory);
   
-  app.post("/stock", urlencodedParser, manageStock);
+  app.post("/stock/update", urlencodedParser, manageStock);
 
-  app.post("/stock_discount", urlencodedParser, manageStock);
-
-  app.post("/delivered_items", urlencodedParser, manageStock);
-
-  app.post("/incoming_order", jsonParser, buildIncomingOrder);
+  app.post("/order/create", jsonParser, buildIncomingOrder);
 };
 
 export default routes;
