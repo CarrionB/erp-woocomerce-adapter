c48521ec9a3c69416a744ec848e6675fba927f75
Bryan Carrion <carrion.ba.dev@gmail.com>
2023-05-10 09:52:13 -0500

    fix: :bug: updated erp login logic

--- src/controllers/erpnext/index.ts
+++ src/controllers/erpnext/index.ts
@@ -4,24 +4,28 @@ import { isCookieInvalid } from "./utils";
 
 const { ERP_URL, ERP_USER, ERP_PASSWORD } = process.env;
 
+export const forceLogin = async () => {
+  const { headers } = await axios({
+    method: "POST",
+    url: `${ERP_URL}/api/method/login`,
+    data: {
+      usr: ERP_USER,
+      pwd: ERP_PASSWORD,
+    },
+    headers: { "Content-Type": "application/json" },
+  });
+  if ("set-cookie" in headers) {
+    const cookieSettings = headers["set-cookie"][0].split(";");
+    const cookieId = cookieSettings[0];
+    const expirationDateString = cookieSettings[1].split("=")[1];
+    storage.setItem("cookieId", cookieId);
+    storage.setItem("expirationDateString", expirationDateString);
+  }
+};
+
 export const erpLogin = async () => {
   if (isCookieInvalid()) {
-    const { headers } = await axios({
-      method: "POST",
-      url: `${ERP_URL}/api/method/login`,
-      data: {
-        usr: ERP_USER,
-        pwd: ERP_PASSWORD,
-      },
-      headers: { "Content-Type": "application/json" },
-    });
-    if ("set-cookie" in headers) {
-      const cookieSettings = headers["set-cookie"][0].split(";");
-      const cookieId = cookieSettings[0];
-      const expirationDateString = cookieSettings[1].split("=")[1];
-      storage.setItem("cookieId", cookieId);
-      storage.setItem("expirationDateString", expirationDateString);
-    }
+    await forceLogin();
   }
 };
 
@@ -35,7 +39,7 @@ export const getConfig = async () => {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Cookie: storedCookie,
+      Cookie: `full_name=Administrator;${storedCookie};system_user=yes;user_id=Administrator`,
     },
   };
   return config;
--- src/controllers/erpnext/utils/index.ts
+++ src/controllers/erpnext/utils/index.ts
@@ -3,13 +3,16 @@ import { storage } from "../../../utilities/storage";
 export const isCookieInvalid = () => {
   const storedCookie = storage.getItem("cookieId");
 
-  if (storedCookie === null) {
+  if (
+    storedCookie === null ||
+    storedCookie === undefined ||
+    storedCookie === ""
+  ) {
     return !Boolean(storedCookie);
   }
 
   const storedExpDateString = storage.getItem("expirationDateString");
   const expirationDate = new Date(storedExpDateString);
   const isExpired = new Date().getTime() > expirationDate.getTime();
-  
   return isExpired;
 };
